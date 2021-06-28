792167cd51d7913287745b8d423ffb78d0ce8533
Jonatha Lucena Neri <jonathanlucenaneri@gmail.com>
2021-06-27 23:48:25 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/jonathanlucena/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.jonathanlucena.cursomc.repositories;
+
+import com.jonathanlucena.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository <Pagamento, Integer> {
+}
---
+++ src/main/java/com/jonathanlucena/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.jonathanlucena.cursomc.repositories;
+
+import com.jonathanlucena.cursomc.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository <Pedido, Integer> {
+}
