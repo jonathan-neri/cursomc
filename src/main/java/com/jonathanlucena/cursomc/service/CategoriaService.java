5fc820af4f636dd72d5c4e540c6aef998eab0ddd
Jonatha Lucena Neri <jonathanlucenaneri@gmail.com>
2021-06-27 21:12:28 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/jonathanlucena/cursomc/service/CategoriaService.java
+++ src/main/java/com/jonathanlucena/cursomc/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.jonathanlucena.cursomc.service;
 
 import com.jonathanlucena.cursomc.domain.Categoria;
 import com.jonathanlucena.cursomc.repositories.CategoriaRepository;
+import com.jonathanlucena.cursomc.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
---
+++ src/main/java/com/jonathanlucena/cursomc/service/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.jonathanlucena.cursomc.service.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    private  static final long serialVerionUID = 1L;
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+
+    public ObjectNotFoundException(String msg, Throwable cause){
+        super(msg,cause);
+    }
+}
